cc34ed7a6db606b44ea90cda7e500b4b952eb963
menomoto <enomotomitsunobu@iMac-2012.local>
2017-11-29 02:15:30 +0900

    second

--- segmentedControl/segmentedControl/UI/SegmentedControlViewController.swift
+++ segmentedControl/segmentedControl/UI/SegmentedControlViewController.swift
@@ -26,7 +26,7 @@ class SegmentedControlViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        navigationItem.title = "価格と個数"
+        navigationItem.title = "価格"
         
         addSubviews()
         addConstraints()
@@ -41,14 +41,17 @@ class SegmentedControlViewController: UIViewController {
     }
     
     fileprivate func configureSubviews() {
+        segmentedControl.tintColor = .gray
         segmentedControl.selectedSegmentIndex = 0
         segmentedControl.addTarget(self, action: #selector(didChangeSellType(_:)), for: .valueChanged)
         changeSellType(index: segmentedControl.selectedSegmentIndex)
 
         sellAuctionTableView.dataSource = self
+        sellAuctionTableView.delegate = self
         sellAuctionTableView.tableFooterView = UIView()
         
         sellFixedTableView.dataSource = self
+        sellFixedTableView.delegate = self
         sellFixedTableView.tableFooterView = UIView()
     }
     
@@ -95,20 +98,74 @@ extension SegmentedControlViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch segmentedControl.selectedSegmentIndex {
-        case 0:
-            return 5
-        case 1:
-            return 3
-        default:
-            return 5
-        }
+        return 2
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            return configureSellAuctionTableView(indexPath: indexPath)
+        case 1:
+            return configureSellFixedTableView(indexPath: indexPath)
+        default:
+            return UITableViewCell()
+        }
+    }
+    
+    fileprivate func configureSellAuctionTableView(indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = Constant.sellType[segmentedControl.selectedSegmentIndex] + "\(indexPath.row)"
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 14.0)
+
+        let yenLabel = UILabel()
+        cell.contentView.addSubview(yenLabel)
+        yenLabel.text = "円"
+        yenLabel.font = UIFont.systemFont(ofSize: 14.0)
+        yenLabel.autoAlignAxis(toSuperviewAxis: .horizontal)
+        yenLabel.autoPinEdge(toSuperviewEdge: .right, withInset: 15.0)
+
+        switch indexPath.row {
+        case 0:
+            cell.textLabel?.text = "開始価格"
+        case 1:
+            cell.textLabel?.text = "即決価格"
+        default:
+            break
+        }
+
+        return cell
+    }
+
+    fileprivate func configureSellFixedTableView(indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 14.0)
+        
+        switch indexPath.row {
+        case 0:
+            cell.textLabel?.text = "即決価格"
+
+            let yenLabel = UILabel()
+            cell.contentView.addSubview(yenLabel)
+            yenLabel.text = "円"
+            yenLabel.font = UIFont.systemFont(ofSize: 14.0)
+            yenLabel.autoAlignAxis(toSuperviewAxis: .horizontal)
+            yenLabel.autoPinEdge(toSuperviewEdge: .right, withInset: 15.0)
+        case 1:
+            cell.textLabel?.text = "値下げ交渉を受け付ける"
+
+            let switchLabel = UISwitch()
+            cell.contentView.addSubview(switchLabel)
+            switchLabel.autoAlignAxis(toSuperviewAxis: .horizontal)
+            switchLabel.autoPinEdge(toSuperviewEdge: .right, withInset: 15.0)
+default:
+            break
+        }
+        
         return cell
     }
 }
 
+extension SegmentedControlViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44.0
+    }
+}
